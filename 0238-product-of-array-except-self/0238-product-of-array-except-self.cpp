b095d59528247679ed2084ff43b9aa050195c967
Arpit Sharma <arpitsharma6105@gmail.com>
2026-03-18 22:32:35 +0530

    space optimization

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<pair<int,int>>vec(n,{1,1});
-        vec[0].first = 1;
-        vec[n-1].second = 1;
+        vector<int>vec(n,1);
+        vec[0]= 1;
         for(int i = 1 ; i < n ; i++){
-            vec[i].first = vec[i-1].first * nums[i-1];
-            int idx = n-i-1;
-            vec[idx].second = vec[idx+1].second * nums[idx+1]; 
+            vec[i] = vec[i-1] * nums[i-1];
         }
-        vector<int>ans;
-        for(int i = 0 ; i < n ; i++){
-            ans.push_back(vec[i].first * vec[i].second);
+        int mul = nums[n-1];
+        for(int i = n-2 ; i>=0 ; i--){
+            vec[i]*=mul;
+            mul*= nums[i];
         }
-        return ans;
+        return vec;
     }
 };
