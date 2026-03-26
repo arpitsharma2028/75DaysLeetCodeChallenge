d55f32100f89aab19a47d344d33cc97bfb31c2e5
Arpit Sharma <arpitsharma6105@gmail.com>
2026-03-26 16:48:01 +0530

    Time: 0 ms (100%), Space: 97.3 MB (59.8%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int currMin = INT_MAX;
-        int profit = 0;
-        for (int i = 0; i < prices.size(); i++) {
-            currMin = min(prices[i], currMin);
-            profit = max(profit, prices[i] - currMin);
+        // profit maximum => sell it when it have maximum amount and purchase it when price is low
+        // also we can't sell before purchasing.
+        int purchase = prices[0];
+        int ans = 0;
+        for(int i = 1 ; i < prices.size() ; i++){
+            ans = max(ans, prices[i]-purchase);
+            purchase = min(purchase , prices[i]);
         }
-        return profit;
+        return ans;
     }
 };
