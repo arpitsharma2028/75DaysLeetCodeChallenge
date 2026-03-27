a822afdb9dc8793a1c6684c084741a5b00309671
Arpit Sharma <arpitsharma6105@gmail.com>
2026-03-27 21:48:38 +0530

    Time: 5 ms (68.11%), Space: 12.7 MB (38.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -16,4 +16,18 @@ public:
         }
         return ans;
     }
+    // int lengthOfLongestSubstring(string s) {
+    //     unordered_set<char>st;
+    //     int l = 0  ,r = 0 , n = s.size(), ans = 0;
+    //     while(r<n){
+    //         while(l<n && st.count(s[r])){
+    //             st.erase(s[l]);
+    //             l++;
+    //         }
+    //         st.insert(s[r]);
+    //         ans = max(ans , r-l+1);
+    //         r++;
+    //     }
+    //     return ans;
+    // }
 };
