92b591f7afaeef7555b988423d3c86a590c011a4
Arpit Sharma <arpitsharma6105@gmail.com>
2026-04-04 22:18:48 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.2%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,7 +4,6 @@ public:
         int l = 0 , r = nums.size()-1;
         while(l<r){
             int mid = l + (r-l)/2;
-            // if(nums[mid] == target)return mid;
             if(nums[mid] < target)l = mid+1;
             else r = mid-1;
         } 
