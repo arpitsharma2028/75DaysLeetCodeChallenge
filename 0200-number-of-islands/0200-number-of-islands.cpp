b5edb21c89934fa04bf97cfc5880f54e8c9a7a7f
Arpit Sharma <arpitsharma6105@gmail.com>
2026-04-06 10:29:36 +0530

    Time: 98 ms (5.06%), Space: 41.5 MB (5.01%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -13,7 +13,7 @@ public:
         // left
         dfs(i , j-1 , graph , vis);
     }
-    void bfs(int i , int j , vector<vector<char>>&graph,vector<vector<bool>>&vis){
+    void bfs(int i , int j , vector<vector<char>>&graph){
         vector<vector<int>>directions = {{1,0} , {0,1} , {-1,0} , {0,-1}};
         int m = graph.size() , n = graph[0].size();
         auto isSafe =  [&](int x , int y){
@@ -23,8 +23,6 @@ public:
         // bfs -> queue -> src  -> push all its unvisited neighbours in it.
         queue<pair<int,int>>q;
         q.push({i,j});
-        vis[i][j] = true;
-
         while(!q.empty()){
             auto [x,y] = q.front();
             q.pop();
@@ -33,24 +31,27 @@ public:
                 int y_ = dir[1];
                 int x_neigh = x + x_;
                 int y_neigh = y+y_;
-                if(isSafe(x_neigh,y_neigh) && !vis[x_neigh][y_neigh] && graph[x_neigh][y_neigh] == '1'){
+                if(isSafe(x_neigh,y_neigh) && graph[x_neigh][y_neigh] == '1'){
                     q.push({x_neigh,y_neigh});
-                    vis[x_neigh][y_neigh] = true;
+                    graph[x_neigh][y_neigh] = '0';
                 }
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size() , n = grid[0].size();
-        vector<vector<bool>>vis(m,vector<bool>(n,false));
+        // vector<vector<bool>>vis(m,vector<bool>(n,false));
         int cnt = 0;
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-                if(!vis[i][j] && grid[i][j] == '1'){
-                    bfs(i,j,grid,vis);
-                    // dfs(i,j,grid , vis);
+                if(grid[i][j] == '1'){
+                    bfs(i,j,grid);
                     cnt++;
                 }
+                // if(!vis[i][j] && grid[i][j] == '1'){
+                //     // dfs(i,j,grid , vis);
+                //     cnt++;
+                // }
             }
         }
         return cnt;
