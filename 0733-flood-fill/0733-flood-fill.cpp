fa6ee965e668dafea6de94d11e4f8087f1544568
Arpit Sharma <arpitsharma6105@gmail.com>
2026-04-06 11:33:21 +0530

    Time: 4 ms (6.04%), Space: 18.5 MB (14.9%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,54 +1,29 @@
 class Solution {
 public:
-    void dfs(int i, int j, vector<vector<int>>& graph,vector<vector<bool>>& vis, int col , int m , int n){
-        if(i < 0 || j < 0 || i >= m || j >= n || graph[i][j] != col || vis[i][j])return;
-        
-        vis[i][j] = true;
-
-        if(i+1 < m){
-            graph[i+1][j] = col;
-            dfs(i+1,j,graph,vis,col,m,n);
-        }
-        if(i-1 >= 0 ){
-            graph[i-1][j] = col;
-            dfs(i-1,j,graph,vis,col,m,n);
-        }
-        if(j+1 < n){
-            graph[i][j+1] = col;
-            dfs(i,j+1,graph,vis,col,m,n);
-        }
-        if(j-1 >= 0){
-            graph[i][j-1] = col;
-            dfs(i,j-1,graph,vis,col,m,n);
-        }
-
-    }
-    void bfs(vector<vector<int>>&image, int i , int j, int color , vector<vector<bool>>&vis){
+    void bfs(vector<vector<int>>&image, int i , int j, int color){
         int m = image.size() , n = image[0].size();
+        int prevCol = image[i][j];
+        if(color == prevCol)return; // important to prevent TLE
         vector<vector<int>>directions = {{1,0} , {0,1} , {-1,0} , {0,-1}};
         auto isSafe =  [&](int x , int y){
             return (x >= 0 && y >= 0 && x < m && y < n);
         };
         queue<pair<pair<int,int> , int>>q; // node , its prev color;
-        int prevCol = image[i][j];
         q.push({{i,j} , prevCol});
-        vis[i][j] = true;
         image[i][j] = color;
         while(!q.empty()){
             int x_u = q.front().first.first;
             int y_u = q.front().first.second;
             prevCol = q.front().second;
-            // image[x_u][y_u] = color;
             q.pop();
 
             for(auto dir : directions){
                 int x_ = dir[0], y_ = dir[1];
                 int x_v = x_u + x_ ;
                 int y_v = y_u + y_ ;
-                if(isSafe(x_v , y_v) &&  !vis[x_v][y_v] && image[x_v][y_v] == prevCol){
+                if(isSafe(x_v , y_v) && image[x_v][y_v] == prevCol){
                     image[x_v][y_v] = color;
                     q.push({{x_v , y_v} , prevCol});
-                    vis[x_v][y_v] = true;
                 }
             }
         }
@@ -56,12 +31,7 @@ public:
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         // M * N
-        int m = image.size() , n = image[0].size();
-        // image[sr][sc] = color;
-        vector<vector<bool>>vis(m,vector<bool>(n,false));
-        bfs(image , sr, sc , color , vis); 
-        return image;
-        dfs(sr,sc,image,vis,color,m,n);
+        bfs(image , sr, sc , color); 
         return image;
     }
 };
