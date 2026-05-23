264a8791812e6c19234bea44d106df4bc6b6f3b3
Arpit <arpitsharma6105@gmail.com>
2026-05-23 22:27:51 +0530

    Time: 0 ms (100%), Space: 9.7 MB (100%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         int n = stones.size();
-        int ans = stones[0];
-        if(n == 1)return ans;
         for(int i = 0 ; i < n && stones.size() > 1 ; i++){
             sort(stones.begin() , stones.end());
             int last = n-i-1  , secLast = n-i-2;
