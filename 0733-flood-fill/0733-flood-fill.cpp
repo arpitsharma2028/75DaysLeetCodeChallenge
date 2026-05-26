33aea8283e243fd696d23dd43a43065684afe1f3
Arpit <arpitsharma6105@gmail.com>
2026-05-26 22:19:42 +0530

    Time: 0 ms (100%), Space: 18.4 MB (31.14%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    void bfs(vector<vector<int>>&image, int i , int j, int color){
-        int m = image.size() , n = image[0].size();
-        int prevCol = image[i][j];
-        if(color == prevCol)return; // important to prevent TLE
-        vector<vector<int>>directions = {{1,0} , {0,1} , {-1,0} , {0,-1}};
-        auto isSafe =  [&](int x , int y){
-            return (x >= 0 && y >= 0 && x < m && y < n);
-        };
-        queue<pair<pair<int,int> , int>>q; // node , its prev color;
-        q.push({{i,j} , prevCol});
-        image[i][j] = color;
-        while(!q.empty()){
-            int x_u = q.front().first.first;
-            int y_u = q.front().first.second;
-            prevCol = q.front().second;
-            q.pop();
-
-            for(auto dir : directions){
-                int x_ = dir[0], y_ = dir[1];
-                int x_v = x_u + x_ ;
-                int y_v = y_u + y_ ;
-                if(isSafe(x_v , y_v) && image[x_v][y_v] == prevCol){
-                    image[x_v][y_v] = color;
-                    q.push({{x_v , y_v} , prevCol});
-                }
-            }
+    void Traversal(vector<vector<int>>&graph, int sr, int sc, int color,vector<vector<bool>>& vis , int m , int n , int color2){
+        if(sr >= m || sc >= n || sc < 0 || sr < 0 || vis[sr][sc])return;
+        vis[sr][sc] = true;
+        // updation
+        if(graph[sr][sc] == color){
+        if(sc+1<n && graph[sr][sc+1] == color2){ graph[sr][sc+1] = color; } // right
+        if(sr+1< m && graph[sr+1][sc] == color2){ graph[sr+1][sc] = color; } // bottom
+        if(sc-1 >= 0 && graph[sr][sc-1] == color2){ graph[sr][sc-1] = color; } // left
+        if(sr-1 >= 0 && graph[sr-1][sc] == color2){ graph[sr-1][sc] = color; } // top
+        // recursive calls
+        
+        Traversal(graph,sr , sc+1, color , vis , m,n , color2); // right
+        Traversal(graph, sr+1 , sc ,color , vis , m,n , color2); // bottom
+        Traversal(graph, sr ,sc-1,color , vis , m,n , color2); // left
+        Traversal(graph, sr-1 , sc,color , vis , m,n , color2); // top
         }
-
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        // M * N
-        bfs(image , sr, sc , color); 
+        int temp = color;
+        color = INT_MAX;
+        int m = image.size() , n = image[0].size(), color2 = image[sr][sc];
+        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        image[sr][sc]=  color;
+        Traversal(image , sr , sc , color ,vis,m , n , color2);
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(image[i][j] == INT_MAX)image[i][j] = temp;
+            }
+        }
         return image;
     }
 };
