1a2fcdd6f824cd8081908568e0b10a7bb7e5fb96
Arpit <arpitsharma6105@gmail.com>
2026-06-03 21:31:00 +0530

    Time: 3 ms (62.2%), Space: 8.4 MB (11.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,29 @@
 class Solution {
+private:
+    int fibb(int n, vector<int>& dp) { // Momoization Approach
+        if (dp[n] != -1)
+            return dp[n];
+        if (n >= 2)
+            dp[n] = (fibb(n - 1, dp) + fibb(n - 2, dp));
+        return dp[n];
+    }
+    int fibTab(int n){ // tablulation approach
+        if(n<2)return n;
+        vector<int> dp(n + 1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++)dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
+    }
 public:
-    int fibb(int n , vector<int> &dp){
-    if( dp[n] != -1)return dp[n];
-    if(n>=2)
-    dp[n] = (fibb(n-1 , dp )+ fibb(n-2  ,dp ));
-    return dp[n];
-}
-int fib(int n) {
-    if(n == 1 || n== 0)return n;
-    vector<int>dp(n+1,-1);
-    dp[0] = 0;
-    dp[1] = 1;
-    return fibb(n , dp);
+    int fib(int n) {
+        return fibTab(n); // tablulation approach
+        
+        if (n == 1 || n == 0)
+            return n;
+        vector<int> dp(n + 1, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        return fibb(n, dp);
     }
 };
