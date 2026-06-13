4ba18ead5eeda4a1e6c249db45ed5a961d5d1e52
Arpit <arpitsharma6105@gmail.com>
2026-06-13 17:02:08 +0530

    Time: 0 ms (100%), Space: 21.6 MB (99.8%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
+    int XORsol (vector<int>&nums){
+        int XOR1 = 0 , XOR2 = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            XOR1 = nums[i]^XOR1;
+            XOR2 = XOR2^(i+1);
+        }
+        return XOR1 ^ XOR2;
+    }
+    int SUMsol(vector<int>&nums){
         int sum = 0;
         int n = nums.size();
         int expSum = n*(n+1)/2;
         for(int i : nums)sum+= i;
         return expSum - sum;
+    }
+    int missingNumber(vector<int>& nums) {
+        return XORsol(nums);
+        return SUMsol(nums);
     }
 };
