7991740c49c4ed43af49ebac14a2d537415ff06e
Arpit <arpitsharma6105@gmail.com>
2026-06-17 16:03:34 +0530

    Time: 0 ms (100%), Space: 7.7 MB (65.27%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
+    int helper(int a , int b){
+        if(b == 0 )return a;
+        return helper(a^b , (a&b)<<1);
+    }
     int getSum(int a, int b) {
+        return helper(a,b);
         while (b != 0) {
             // 1. Find the carry bits
             // We cast to unsigned int to prevent overflow runtime errors in C++
